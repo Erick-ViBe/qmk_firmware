57c375309da46375bc57bff72010882a46082790
Erick-ViBe <erickvb12@gmail.com>
2020-12-13 18:15:05 -0600

    First Keymap

--- keyboards/crkbd/keymaps/Erick-2-ViBe/keymap.c
+++ keyboards/crkbd/keymaps/Erick-2-ViBe/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_PWR, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_BRID,                        MACRO1,  MACRO2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         XXXXXXX, KC_TRNS, KC_SPC,    KC_ENT, RAISE, KC_LALT
+                                         KC_LSFT, KC_TRNS, KC_SPC,    KC_ENT, RAISE, KC_LALT
                                       //|--------------------------|  |--------------------------|
   ),
 
